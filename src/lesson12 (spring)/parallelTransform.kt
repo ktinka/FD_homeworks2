f786762352f02c62e9a89d81cdaf67ceeb828f56
ktinka <159685174+ktinka@users.noreply.github.com>
2026-05-22 16:21:58 +0300

    solution of the task "parallelTransform"

--- src/lesson12 (spring)/parallelTransform.kt
+++ src/lesson12 (spring)/parallelTransform.kt
@@ -9,9 +9,23 @@ package homework
  * @param transform функция преобразования
  * @return список преобразованных элементов в исходном порядке
  */
+
+// Решение - параллельность преобразований
+
+package homework
+
+import kotlinx.coroutines.*
+
 suspend fun <T, R> parallelTransform(
     items: List<T>,
     transform: suspend (T) -> R
-): List<R> {
-    TODO("Реализуйте параллельное преобразование с использованием async/await")
+): List<R> = coroutineScope {
+    
+    val deferredResults = items.map { item ->
+        async(Dispatchers.Default) {
+            transform(item)
+        }
+    }
+    
+    deferredResults.map { it.await() }
 }
